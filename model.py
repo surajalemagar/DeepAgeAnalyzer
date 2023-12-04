84d0525cb1091310eabeff8806c8e3a30561947f
surajalemagar <surajido111@gmail.com>
2023-12-04 15:48:06 +0000

    first model

--- model.py
+++ model.py
@@ -20,3 +20,224 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from PIL import Image
+
+transform = transforms.Compose([
+            transforms.Resize(224),             # resize shortest side to 224 pixels
+            transforms.CenterCrop(224),         # crop longest side to 224 pixels at center            
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ])
+
+
+data=pd.read_csv('/home/suraj/Desktop/DeepAgeAnalyzer/train.csv')
+class_names0=data['Class'].unique().tolist()
+class_names=['YOUNG','MIDDLE','OLD']
+N=list(range(len(class_names)))
+normal_mapping=dict(zip(class_names,N)) 
+reverse_mapping=dict(zip(N,class_names))       
+data['label']=data['Class'].map(normal_mapping)
+dir0='/home/suraj/Desktop/DeepAgeAnalyzer/Train'
+data['path']=data['ID'].apply(lambda x:os.path.join(dir0,x))
+
+def create_path_label_list(df):
+    path_label_list = []
+    for _, row in df.iterrows():
+        path = row['path']
+        label = row['label']
+        path_label_list.append((path, label))
+    return path_label_list
+
+path_label = create_path_label_list(data)
+path_label = random.sample(path_label,1000)
+print(path_label[0:3])
+
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, path_label, transform=None):
+        self.path_label = path_label
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.path_label)
+
+    def __getitem__(self, idx):
+        path, label = self.path_label[idx]
+        img = Image.open(path).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, label
+
+class ImageDataset(pl.LightningDataModule):
+    def __init__(self, path_label, batch_size=32):
+        super().__init__()
+        self.path_label = path_label
+        self.batch_size = batch_size
+        self.transform = transforms.Compose([
+            transforms.Resize(224),             # resize shortest side to 224 pixels
+            transforms.CenterCrop(224),         # crop longest side to 224 pixels at center            
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ])
+
+    def setup(self, stage=None):
+        dataset = CustomDataset(self.path_label, self.transform)
+        dataset_size = len(dataset)
+        print(dataset_size)
+        train_size = int(0.8 * dataset_size) 
+        test_size = dataset_size - train_size
+
+        self.train_dataset = torch.utils.data.Subset(dataset, range(train_size))
+        self.test_dataset = torch.utils.data.Subset(dataset, range(train_size, dataset_size))
+
+    def __len__(self):
+        if self.train_dataset is not None:
+            return len(self.train_dataset)
+        elif self.test_dataset is not None:
+            return len(self.test_dataset)
+        else:
+            return 0
+
+    def __getitem__(self, index):
+        if self.train_dataset is not None:
+            return self.train_dataset[index]
+        elif self.test_dataset is not None:
+            return self.test_dataset[index]
+        else:
+            raise IndexError("Index out of range. The dataset is empty.")
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+
+    #def val_dataloader(self):
+        #return DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+class DataModule(pl.LightningDataModule):
+    
+    def __init__(self, transform=transform, batch_size=32):
+        super().__init__()
+        self.root_dir = "/kaggle/input/faces-age-detection-dataset/Train"
+        self.transform = transform
+        self.batch_size = batch_size
+
+    def setup(self, stage=None):
+        #dataset = datasets.ImageFolder(root=self.root_dir, transform=self.transform)
+        n_data = len(dataset)
+        n_train = int(0.8 * n_data)
+        n_test = n_data - n_train
+
+        train_dataset, test_dataset =  random_split(dataset, [n_train, n_test])
+
+        self.train_dataset = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        self.test_dataset = DataLoader(test_dataset, batch_size=self.batch_size)
+
+    def train_dataloader(self):
+        return self.train_dataset
+
+    def test_dataloader(self):
+        return self.test_dataset
+
+class ConvolutionalNetwork(LightningModule):
+    
+    def __init__(self):
+        super(ConvolutionalNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
+        self.fc1 = nn.Linear(16 * 54 * 54, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 20)
+        self.fc4 = nn.Linear(20, len(class_names))
+
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 16 * 54 * 54)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = F.relu(self.fc3(X))
+        X = self.fc4(X)
+        return F.log_softmax(X, dim=1)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        return optimizer
+
+    def training_step(self, train_batch, batch_idx):
+        X, y = train_batch
+        y_hat = self(X)
+        loss = F.cross_entropy(y_hat, y)
+        pred = y_hat.argmax(dim=1, keepdim=True)
+        acc = pred.eq(y.view_as(pred)).sum().item() / y.shape[0]
+        self.log("train_loss", loss)
+        self.log("train_acc", acc)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        X, y = val_batch
+        y_hat = self(X)
+        loss = F.cross_entropy(y_hat, y)
+        pred = y_hat.argmax(dim=1, keepdim=True)
+        acc = pred.eq(y.view_as(pred)).sum().item() / y.shape[0]
+        self.log("val_loss", loss)
+        self.log("val_acc", acc)
+
+    def test_step(self, test_batch, batch_idx):
+        X, y = test_batch
+        y_hat = self(X)
+        loss = F.cross_entropy(y_hat, y)
+        pred = y_hat.argmax(dim=1, keepdim=True)
+        acc = pred.eq(y.view_as(pred)).sum().item() / y.shape[0]
+        self.log("test_loss", loss)
+        self.log("test_acc", acc)
+
+if __name__ == '__main__':
+    dataset = ImageDataset(path_label)
+    dataset.setup() 
+    train_dataloader = dataset.train_dataloader()
+    test_dataloader = dataset.test_dataloader()
+    datamodule = DataModule()
+    datamodule.setup()
+
+model = ConvolutionalNetwork()
+trainer = pl.Trainer(max_epochs=30)
+trainer.fit(model, datamodule)
+
+datamodule.setup(stage='test')
+test_loader = datamodule.test_dataloader()
+trainer.test(dataloaders=test_loader)
+
+for images, labels in datamodule.train_dataloader():
+    break
+im=make_grid(images,nrow=16)
+
+plt.figure(figsize=(12,12))
+plt.imshow(np.transpose(im.numpy(),(1,2,0)))
+
+inv_normalize=transforms.Normalize(mean=[-0.485/0.229,-0.456/0.224,-0.406/0.225],
+                                   std=[1/0.229,1/0.224,1/0.225])
+im=inv_normalize(im)
+
+plt.figure(figsize=(12,12))
+plt.imshow(np.transpose(im.numpy(),(1,2,0)))
+
+device = torch.device("cpu")   #"cuda:0"
+
+model.eval()
+y_true=[]
+y_pred=[]
+with torch.no_grad():
+    for test_data in datamodule.test_dataloader():
+        test_images, test_labels = test_data[0].to(device), test_data[1].to(device)
+        pred = model(test_images).argmax(dim=1)
+        for i in range(len(pred)):
+            y_true.append(test_labels[i].item())
+            y_pred.append(pred[i].item())
+
+print(classification_report(y_true,y_pred,target_names=class_names,digits=4))
